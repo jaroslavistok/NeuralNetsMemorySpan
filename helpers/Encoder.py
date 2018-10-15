10625bd5c3cf540556511df8d9975b134ad9e2ea
Jaroslav Ištok <jaroslav.istok@gmail.com>
2018-10-15 22:18:47 +0200

    Added characters encoder

---
+++ helpers/Encoder.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+"""
+abcdefghijklmnopqrstuvwxyz
+
+"""
+
+
+class Encoder:
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    @staticmethod
+    def encode_character(character):
+        encoded_character = np.zeros(26)
+        position = Encoder.alphabet.find(character)
+        encoded_character[position] = 1
+        return encoded_character
+
+    @staticmethod
+    def decode_character(encoded_character):
+        index = encoded_character.tolist().index(1)
+        return Encoder.alphabet[index]
