d8816824f5173bc63a2ae9987b125e6a4fd7f0bf
Jaroslav Ištok <jaroslav.istok@gmail.com>
2018-12-09 09:31:12 +0100

    Added bibliography

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 """
 Main file
 """
+import numpy as np
+
+
+from helpers.Encoder import Encoder
+from helpers.LongestCommonSubsequence import LongestCommonSubsequence
+
+sequences = ["brownasdfoersjumps",
+             "foxsxzxasis12sa[[#brown",
+             "thissasbrownxc-34a@s;"]
+
+lcs = LongestCommonSubsequence()
+print(lcs.get_longest_subsequence(sequences))
+print(lcs.get_longest_subsequence_length(sequences))
+
