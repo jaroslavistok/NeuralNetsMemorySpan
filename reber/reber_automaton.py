05caf3b2e8e760fe39ae3fbc88a8b1000b4415c4
Jaroslav Ištok <jaroslav.istok@gmail.com>
2019-02-02 21:36:24 +0100

    Added simple reber automata string generator

---
+++ reber/reber_automaton.py
@@ -0,0 +1,30 @@
+import random
+
+
+class ReberAutomaton:
+    def __init__(self):
+        self.states = self._create_states()
+
+    def generate_reber_string(self):
+        generated_string = ''
+        current_state = self.states[0]
+        while True:
+            random_transition = random.randint(0, 1)
+            if len(current_state) < 2:
+                random_transition = 0
+            transition = current_state[random_transition]
+            for next_state, character in transition.items():
+                generated_string += character
+                current_state = self.states[next_state]
+                if next_state == -1:
+                    return generated_string
+
+    def _create_states(self):
+        states = [[{1: 'T'}, {2: 'P'}],
+                  [{1: 'S'}, {3: 'X'}],
+                  [{2: 'T'}, {4: 'V'}],
+                  [{5: 'S'}, {2: 'X'}],
+                  [{3: 'P'}, {5: 'V'}],
+                  [{-1: 'E'}]]
+        return states
+
---
+++ reber/reber_automaton_runner.py
@@ -0,0 +1,4 @@
+from reber.reber_automaton import ReberAutomaton
+
+reber_automaton = ReberAutomaton()
+print(reber_automaton.generate_reber_string())
